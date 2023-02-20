5bec5427de6a09b08b83eba029c9a0023c1e63a8
Wangderx <112056740+Wangderx@users.noreply.github.com>
2023-02-20 16:51:51 +0800

    pull test

--- src/main/java/cn/wangderx/git/GitTest.java
+++ src/main/java/cn/wangderx/git/GitTest.java
@@ -18,5 +18,6 @@ public class GitTest {
         System.out.println("hello git!3");
         System.out.println("hello git!3");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
