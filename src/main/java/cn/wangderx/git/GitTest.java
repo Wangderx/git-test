8bd7d88c3bfce1cef2b5e453126094cb50f8eb8b
wangderx <wangderx@189.cn>
2023-02-20 11:44:42 +0800

    second commit

---
+++ src/main/java/cn/wangderx/git/GitTest.java
@@ -0,0 +1,12 @@
+package cn.wangderx.git;
+
+/**
+ * @Author Wangderx
+ * @Date 2023/2/20 11:36
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git!2");
+    }
+}
